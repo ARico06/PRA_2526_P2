9cb07bd3dc8e05d2d5a3f16bf365e9222431cfb3
ARICALA <aricala@alumno.upv.es@ldsic-vdi15.upvnet.upv.es>
2025-10-27 15:53:46 +0100

    actualizacion intermedia

--- DyV.h
+++ DyV.h
@@ -31,3 +31,35 @@ int BusquedaBinaria_INV(T x, vector<T> v, int ini, int fin){
 		return BusquedaBinaria(x, v, medio+1, fin);
 	}
 }
+
+template<typename T>
+int Partition( vector<T> &vect, int ini, int fin){
+	T x = vect[fin];
+	int i = ini;
+	T aux;
+	for(int j = ini; j<fin-1; j++){
+		if(vect[j] <= x){
+			aux = vect[j];
+			vect[j] = vect[i];
+			vect[i] = aux;
+			i++;
+		}	
+	}
+	aux = vect[fin];
+	vect[fin] = vect[i];
+	vect[i] = aux;
+	return i;	
+}
+
+template<typename T>
+int QuickSort( vector<T> &vect, int ini, int fin){
+	if(ini < fin){
+		int pivot = Partition(vect, ini, fin);
+		QuickSort(vect, ini, pivot-1);
+		QuickSort(vect, pivot+1);
+	}else{
+		return -1;
+	}
+}
+
+
